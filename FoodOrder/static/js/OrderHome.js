c1eee3e31a2403990d7d673e23ff5925c0dfc423
nuso <nelohjb@hotmail.com>
2019-06-24 09:16:50 +0800

    OrderHome: 添加减少商品数量.

--- FoodOrder/static/js/OrderHome.js
+++ FoodOrder/static/js/OrderHome.js
@@ -24,12 +24,12 @@ $(document).on("click", ".FoodItem", function() {
                         <div hidden class="foodPrice">{1}</div>\
                         <div class="input-group input-group-sm col-4">\
                             <div class="input-group-prepend">\
-                                <button class="btn input-group-text" type="button">-</button>\
+                                <button class="btn SubFood input-group-text" type="button">-</button>\
                             </div>\
-                            <input type="text" class="form-control text-center" value="1"\
+                            <input type="text" class="FoodAmount form-control text-center" value="1"\
                                 style="max-width: 50px;">\
                             <div class="input-group-append">\
-                                <button class="btn input-group-text" type="button">+</button>\
+                                <button class="btn AddFood input-group-text" type="button">+</button>\
                             </div>\
                         </div>\
                         <div class="col-2 p-0"><span class="aligin-middle"\
@@ -45,11 +45,70 @@ $(document).on("click", ".FoodItem", function() {
             $("#OrderList").append(html);
         });
 
+    // 商品添加数量
+    $(".AddFood")
+        .off("click")
+        .click(function() {
+            var input = $(this)
+                .parent()
+                .parent()
+                .find("input");
+            if (input.val() == "") {
+                input.val(1);
+                return;
+            } else if (parseInt(input.val()) >= 99) {
+                input.val(99);
+                return;
+            }
+            input.val(parseInt(input.val()) + 1);
+        });
+
+    // 商品减少数量
+    $(".SubFood")
+        .off("click")
+        .click(function() {
+            var input = $(this)
+                .parent()
+                .parent()
+                .find("input");
+            if (parseInt(input.val()) <= 1 || input.val() == "") {
+                input.val(1);
+                return;
+            }
+            input.val(parseInt(input.val()) - 1);
+            if (parseInt(input.val()) >= 99) {
+                input.val(99);
+                return;
+            }
+        });
+
     // 绑定删除商品按钮
     $(".DeleteItemBtn")
         .off("click")
         .click(function() {
-            var item = $(this).parent().parent().parent();
+            var item = $(this)
+                .parent()
+                .parent()
+                .parent();
             item.remove();
         });
+
+    // 限制数量输入 暂仅限制输入为数字
+    $(".FoodAmount")
+        .keyup(function() {
+            $(this).val(
+                $(this)
+                    .val()
+                    .replace(/[^0-9]/g, "")
+            );
+        })
+        .bind("paste", function() {
+            // CTR+V事件处理
+            $(this).val(
+                $(this)
+                    .val()
+                    .replace(/[^0-9]/g, "")
+            );
+        })
+        .css("ime-mode", "disabled"); // CSS设置输入法不可用
 });
