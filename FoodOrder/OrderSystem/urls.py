d46a288c6b5cc837ef3e1a8be3d542a26f59920d
nuso <nelohjb@hotmail.com>
2019-06-25 20:28:55 +0800

    order check: 待支付账单页面.

--- FoodOrder/OrderSystem/urls.py
+++ FoodOrder/OrderSystem/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.OrderHome, name='OrderHome'),
+    path('check', views.CheckUnpaidOrder),
     re_path(r'q(?P<order_id>[\d]+)', views.QueryOrder),
 
 ]
