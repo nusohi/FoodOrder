2ffda66f054877d175e15dd91b6824af37244f33
nuso <nelohjb@hotmail.com>
2019-06-25 20:29:29 +0800

    待支付页面 View.

--- FoodOrder/OrderSystem/views.py
+++ FoodOrder/OrderSystem/views.py
@@ -24,20 +24,30 @@ def OrderHome(request):
         table_id = request.POST.get('table')
 
         # 创建订单
-        new_order = Order(table=table_id, is_pay=False)
-        new_order.save()
+        new_order = Order(table_id=table_id, is_pay=False)
+        new_order.save()    # 先 save 再获取 ID
         order_id = new_order.ID
+        food_amount = 0
+        total_price = 0
 
         for food in foodList:
             curFood = Food.objects.get(pk=food['id'])
             price = curFood.price
             sum_price = price * food['amount']
+
+            food_amount += food['amount']
+            total_price += sum_price
+
             OrderItem.objects.create(
                 orderID=new_order,
                 foodID=curFood,
                 amount=food['amount'],
                 sum_price=sum_price
             )
+        # 订单的物品总数、总价
+        new_order.food_amount = food_amount
+        new_order.total_price = total_price
+        new_order.save()
 
         return HttpResponse(json.dumps({
             'order_id': order_id
@@ -53,7 +63,7 @@ def QueryOrder(request, order_id):
     foodList = Food.objects.filter(orderitem__orderID__ID=order_id)
 
     with connection.cursor() as cursor:
-        SELECT_COL = 'OrderSystem_food.ID, OrderSystem_food.title, OrderSystem_orderitem.amount'
+        SELECT_COL = 'OrderSystem_food.ID ID, OrderSystem_food.title title, OrderSystem_orderitem.amount amount'
         SELECT_COL += ', OrderSystem_orderitem.sum_price '
         SELECT_FROM = 'OrderSystem_food, OrderSystem_orderitem '
         SELECT_WHERE = 'OrderSystem_food.ID=OrderSystem_orderitem.foodID_id '
@@ -61,21 +71,35 @@ def QueryOrder(request, order_id):
             order_id)
         cursor.execute(
             f'select {SELECT_COL} from {SELECT_FROM} where {SELECT_WHERE}')
-        foodList = cursor.fetchall()
-        foodJsonList = []
-        for food in foodList:
-            foo = {}
-            foo['ID'] = food[0]
-            foo['title'] = food[1]
-            foo['amount'] = food[2]
-            foo['sum_price'] = food[3]
-            foodJsonList.append(foo)
+        foodJsonList = dictfetchall(cursor)
 
-    return render(
-        request,
-        'QueryOrder.html',
-        {
-            'order': order,
-            'foodList': foodJsonList,
-        }
-    )
+    return render(request, 'QueryOrder.html', {
+        'order': order,
+        'foodList': foodJsonList,
+    })
+
+
+# 待结账页面
+def CheckUnpaidOrder(request):
+    # 查询当前未结账订单
+    orderList = []
+    with connection.cursor() as cursor:
+        SELECT_COL = 'ID, create_time, table_id, total_price'
+        SELECT_FROM = 'OrderSystem_order'
+        SELECT_WHERE = 'is_pay=0'   # 0 false
+        SELECT = f'select {SELECT_COL} from {SELECT_FROM} where {SELECT_WHERE}'
+        cursor.execute(SELECT)
+        orderList = dictfetchall(cursor)
+
+    return render(request, 'Checkout.html', {
+        'orderList': orderList,
+    })
+
+
+def dictfetchall(cursor):
+    '''辅助函数 数据库查询结果转换成 json/dict'''
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
